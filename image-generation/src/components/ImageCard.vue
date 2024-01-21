dd27caa25d72fb55591be25944b06611b40f4c09
Aniket Maurya <theaniketmaurya@gmail.com>
2024-01-21 23:02:20 +0000

    update

--- image-generation/src/components/ImageCard.vue
+++ image-generation/src/components/ImageCard.vue
@@ -1,14 +1,15 @@
 <template>
     <div class="flex justify-center items-center h-screen">
         <div class="w-auto h-auto rounded-md border">
-            <img src="https://images.unsplash.com/photo-1522199755839-a2bacb67c546?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGJsb2d8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
-                alt="Laptop" class="h-[200px] w-full rounded-t-md object-cover" />
+            <img :src="imagePath" alt="Generated Image" class="h-full w-full rounded-t-md object-cover" />
             <div class="p-4">
-                <p class="mt-3 text-sm text-gray-600">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-                    debitis?
-                </p>
-                <button type="button"
+                <div class="w-full">
+                    <input v-model="inputPrompt"
+                        class="flex h-10 w-full rounded-md border border-black/30 bg-transparent px-3 py-2 text-sm placeholder:text-gray-600 focus:outline-none focus:ring-1 focus:ring-black/30 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50"
+                        type="text" placeholder="Image prompt"/>
+                </div>
+
+                <button type="button" @click="generateImage()"
                     class="mt-4 w-full rounded-sm bg-black px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black">
                     Generate
                 </button>
@@ -25,8 +26,47 @@ export default {
     name: "ImageCard",
     data() {
         return {
-            imagePath: sampleImageSrc
-        };
+            imagePath: sampleImageSrc,
+            inputPrompt: "dog in a frosty background, puppy eyes, cute, full of lights, 8K"
+        }
+    },
+    methods: {
+        generateImage() {
+            if (this.inputPrompt.length > 1) {
+                this.postRequest(this.inputPrompt);
+            }
+        },
+        async postRequest(newPrompt) {
+            try {
+                const response = await fetch('http://127.0.0.1:8000/endpoint', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        // Add any additional headers if needed
+                    },
+                    body: JSON.stringify({
+                        // Your request payload goes here
+                        prompt: newPrompt,
+                        negative_prompt: 'ugly, blurry, poor quality',
+                    }),
+                });
+
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+
+                // Assuming the response is an image, convert it to a data URL
+                const responseDataUrl = await response.blob().then((blob) =>
+                    URL.createObjectURL(blob)
+                );
+
+                this.imagePath = responseDataUrl;
+                return responseDataUrl
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                // Handle error appropriately (e.g., show an error message to the user)
+            }
+        }
     }
 }
 </script>
