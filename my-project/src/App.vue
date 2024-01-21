5b8965281d3edc118f322bcc85afb389794598d6
Aniket Maurya <theaniketmaurya@gmail.com>
2024-01-21 19:34:50 +0000

    update

---
+++ my-project/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Hii from Aniket</h1>
+    <TodoList/>
+  </template>
+  
+  
+  <script>
+  import TodoList from './components/TodoList.vue';
+  
+  export default {
+    name: "app",
+    components: {
+      TodoList
+    }
+  
+  }
+  </script>
+  
+  <style>
+  body {
+    background-color: rgb(66,66,66);
+    color: aliceblue;
+  }
+  </style>
+  
---
+++ my-project/src/components/TodoList.vue
@@ -0,0 +1,56 @@
+<template>
+    <h1>ToDo List</h1>
+    <div>
+        <ul>
+            <TodoItem v-for="(todo, index) in todos" :key="index" 
+                :todoString="todo.todoString" 
+                :completed="todo.completed"
+            />
+        </ul>
+
+    
+</div>
+
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue';
+
+export default {
+    name: "TodoList",
+    components: {
+        TodoItem
+    },
+
+    data() {
+        return {
+            todos: [
+                { todoString: "Take dog for walk", completed: true },
+                { todoString: "Drink water", completed: false },
+                { todoString: "Cook dinner", completed: false },
+                { todoString: "Learn Vue", completed: true }
+
+            ]
+
+        }
+    },
+    methods: {
+        addTodo(newTodo) {
+            this.todos.push({ todoString: newTodo, completed: false })
+
+        },
+        toggleTodo(todo) {
+            todo.completed = !todo.completed;
+        },
+        editTodo(todo, newTodoString) {
+            todo.todoString = newTodoString
+
+        },
+        deleteTodo(deleteTodo) {
+            this.todos = this.todos.filter(todo => todo !== deleteTodo);
+        }
+
+    },
+}
+</script>
+
